9b5ee91b1c959778080ba9db76ee6d78272c432e
jessi_alarcon <jessicala182@gmail.com>
2021-07-25 18:05:00 -0400

    Primer commit de Jessica Alarcon

--- tarea1/index.js
+++ tarea1/index.js
@@ -1,7 +1,13 @@
-const { mergeSort } = require('./src/algorithm/sortAlgorithms')
+const { mergeSort, bubbleSort } = require('./src/algorithm/sortAlgorithms')
 
 const listaDesordenada = [5, 7, 6, 1, 4, 2, 3, 9, 8]
 console.log(`Lista desordenada: `, listaDesordenada)
 
 let listaOrdenada = mergeSort(listaDesordenada)
 console.log(`Lista ordenada por MergeSort: `, listaOrdenada)
+
+const listaDesordenada2 = [5, 7, 6, 1, 4, 2, 3, 9, 8, 22]
+console.log(`Lista desordenada: `, listaDesordenada2)
+
+let listaOrdenada2 = bubbleSort(listaDesordenada2)
+console.log(`Lista ordenada por BubbleSort: `, listaOrdenada2)
--- tarea1/src/algorithm/sortAlgorithms.js
+++ tarea1/src/algorithm/sortAlgorithms.js
@@ -14,4 +14,20 @@ sortAlgorithms.mergeSort = (array) => {
     return merge(sortAlgorithms.mergeSort(left), sortAlgorithms.mergeSort(right))
 }
 
+sortAlgorithms.bubbleSort = (array) => {
+    let swapped = true;
+    do {
+        swapped = false;
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] > array[j + 1]) {
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return array;
+}
+
 module.exports = sortAlgorithms
